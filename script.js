34863888db60757d435717505b1985237dc10a14
Noah Fallu <hyperank.contact@gmail.com>
2025-07-09 19:50:11 -0600

    a

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+  import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.5/+esm';
+  
   const supabaseUrl = 'https://rrnucumzptbwdxtkccyx.supabase.co';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJybnVjdW16cHRid2R4dGtjY3l4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxMDcxNDAsImV4cCI6MjA2NzY4MzE0MH0.HH923Txx1G6YXlJcKaDFVpEBK6WuLRT7adqQRi_Isj0';
   const supabase = supabase.createClient(supabaseUrl, supabaseKey);
